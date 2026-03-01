7c0970e60fb9bcd51dab7e5e1202284bc92d9b12
Sherlock <9hoshi2004@gmail.com>
2026-03-01 17:51:43 +0700

    Updated Scripts

--- Program.cs
+++ Program.cs
@@ -2,10 +2,7 @@
 builder.Services.AddRazorPages(); // เปิดใช้งาน Razor Pages
 
 var app = builder.Build();
-
-app.UseStaticFiles(); // บอกให้ระบบวิ่งไปหาไฟล์ในโฟลเดอร์ wwwroot เช่น css, js, images เป็นต้น
 app.UseRouting(); // บอกให้ระบบวิ่งไปหาไฟล์ในโฟลเดอร์ Pages และ Controllers เพื่อทำการ Routing ไปยังหน้าเว็บที่ต้องการ
-
 app.MapRazorPages(); // บอกให้ระบบวิ่งไปหาไฟล์ในโฟลเดอร์ Pages
 
 app.Run();
